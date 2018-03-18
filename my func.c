b1e56cdcc1e27f818f765a3b210fa15dd6d6a13c
MMMOWWW <36041208+mmmowww@users.noreply.github.com>
2018-03-18 19:22:33 +0300

    Add files via upload

---
+++ my func.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<conio.h>
+int sumi(int a, int b)
+{
+int c = 0;
+c = a + b;
+return c;
+}
+ int main()
+{
+int j = 56;
+int k = 78;
+      sumi(j,k);
+      printf(sumi(j,k));
+      scanf(&k);
+
+
+}
